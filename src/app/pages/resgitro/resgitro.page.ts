068835c931ee60cde0b9528344b16913ffbb6db7
Tomas Barrueto <134245189+ToBarrueto@users.noreply.github.com>
2023-09-02 01:07:15 -0400

    se agregan nuevas paginas y diseño

--- src/app/pages/resgitro/resgitro.page.ts
+++ src/app/pages/resgitro/resgitro.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-resgitro',
-  templateUrl: './resgitro.page.html',
-  styleUrls: ['./resgitro.page.scss'],
+  selector: 'app-resgitro', // Cambia 'app-registro' por 'app-resgitro'
+  templateUrl: 'resgitro.page.html', // Cambia 'registro.page.html' por 'resgitro.page.html'
+  styleUrls: ['resgitro.page.scss'], // Cambia 'registro.page.scss' por 'resgitro.page.scss'
 })
-export class ResgitroPage implements OnInit {
+export class ResgitroPage { // Cambia 'RegistroPage' por 'ResgitroPage'
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  registrarse() {
+    // Redirige a la página de inicio de sesión cuando se hace clic en el botón
+    this.router.navigate(['/login']);
   }
-
 }
