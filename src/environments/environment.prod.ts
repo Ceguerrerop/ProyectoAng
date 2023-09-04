75cbb58c9d8805227ffba5a2a7874df85ac29a39
Ceguerrerop <134183626+Ceguerrerop@users.noreply.github.com>
2023-09-04 11:11:37 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production:true ,
+  firebaseConfig : {
+    apiKey: "AIzaSyAa1-VbUMuDp26w-34GQwY-0feu_QIezIU",
+    authDomain: "proyectoang-asiste.firebaseapp.com",
+    projectId: "proyectoang-asiste",
+    storageBucket: "proyectoang-asiste.appspot.com",
+    messagingSenderId: "691415879903",
+    appId: "1:691415879903:web:3894303ebbf81f5c6d996a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAa1-VbUMuDp26w-34GQwY-0feu_QIezIU",
+    authDomain: "proyectoang-asiste.firebaseapp.com",
+    projectId: "proyectoang-asiste",
+    storageBucket: "proyectoang-asiste.appspot.com",
+    messagingSenderId: "691415879903",
+    appId: "1:691415879903:web:3894303ebbf81f5c6d996a"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
